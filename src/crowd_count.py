547665195fa03d864f2729cc8096f65e8217f6fe
benntay1 <63178445+benntay1@users.noreply.github.com>
2020-10-11 02:18:23 +0800

    syncing src files as per smc repo

--- src/crowd_count.py
+++ src/crowd_count.py
@@ -1,28 +1,37 @@
 import torch.nn as nn
-import network
-from network import *
-from models import *
+from models.smc.src import network
+from models.smc.src.model import SMC
 
 
 class CrowdCounter(nn.Module):
-    def __init__(self, architecture):
-        super(CrowdCounter, self).__init__()
-
-        if architecture == 'csrnet': self.DME = CSRNet()
-        elif architecture == 'mcnn': self.DME = MCNN()
-        
+    def __init__(self, is_cuda=False):
+        super(CrowdCounter, self).__init__()        
+        self.model = SMC(vary=False)
         self.loss_fn = nn.MSELoss()
+        self.is_cuda=is_cuda
         
     @property
     def loss(self):
         return self.loss_mse
-    
+
     def forward(self, im_data, gt_data=None):        
-        im_data = network.np_to_variable(im_data, is_cuda=False, is_training=self.training)
-        density_map = self.DME(im_data)
+        im_data = network.np_to_variable(
+            im_data, 
+            is_cuda=self.is_cuda, 
+            is_training=self.training
+        )
+
+        density_map = self.model(im_data)
+#         print(f'Density map size: {density_map.shape}. Density map type: {density_map.dtype}')
+
         
         if self.training:                        
-            gt_data = network.np_to_variable(gt_data, is_cuda=False, is_training=self.training)
+            gt_data = network.np_to_variable(
+                gt_data, 
+                is_cuda=self.is_cuda, 
+                is_training=self.training
+            )
+#             print(f'Ground Truth Map Size: {gt_data.shape}. Ground truth map type: {gt_data.dtype}')
             self.loss_mse = self.build_loss(density_map, gt_data)
             
         return density_map
