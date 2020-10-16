23e3ffeacd78347b4d0c7a27b05cdd348e5a7e70
benntay1 <63178445+benntay1@users.noreply.github.com>
2020-10-16 21:18:25 +0800

    cleaned up crowd_count.py

--- src/crowd_count.py
+++ src/crowd_count.py
@@ -6,13 +6,12 @@ class CrowdCounter(nn.Module):
     def __init__(self, is_cuda=False):
         super(CrowdCounter, self).__init__()        
         self.model = CSRNet()
-        #self.loss_fn = nn.MSELoss()
-        self.loss_fn = nn.SmoothL1Loss()
+        self.criterion = nn.SmoothL1Loss()
         self.is_cuda=is_cuda
         
     @property
     def loss(self):
-        return self.loss_mse
+        return self.loss
 
     def forward(self, im_data, gt_data=None):        
         im_data = network.np_to_variable(
@@ -32,10 +31,10 @@ class CrowdCounter(nn.Module):
                 is_training=self.training
             )
 
-            self.loss_mse = self.build_loss(density_map, gt_data)
+            self.loss = self.build_loss(density_map, gt_data)
             
         return density_map
     
     def build_loss(self, density_map, gt_data):
-        loss = self.loss_fn(density_map, gt_data)
+        loss = self.criterion(density_map, gt_data)
         return loss
